c573d434c933a524d1cca13493e50ad0f4386ad1
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 13:27:43 +0300

    added categories

--- Apps.AmazonComprehend/AmazonComprehendApplication.cs
+++ Apps.AmazonComprehend/AmazonComprehendApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.AmazonComprehend;
 
-public class AmazonComprehendApplication : IApplication
+public class AmazonComprehendApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.ArtificialIntelligence, ApplicationCategory.AmazonApps];
+        set { }
+    }
+    
     public string Name
     {
         get => "Amazon Comprehend";
